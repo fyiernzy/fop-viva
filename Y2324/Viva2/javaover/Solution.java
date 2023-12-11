a276df1d5a105a30c3edf15f646280cf1d3d32b5
Siti Hajar <sitihajarazman48@gmail.com>
2023-12-12 07:44:38 +0800

    <Message>

---
+++ Y2324/Viva2/javaover/Solution.java
@@ -0,0 +1,46 @@
+public class Solution {
+
+    public double[][] addition(double[][] a, double[][] b) {
+        // Implement this method
+        return null;
+    }
+
+    public double[][] subtraction(double[][] a, double[][] b) {
+        // Implement this method
+        return null;
+    }
+
+    public double[][] multiplication(double[][] a, double[][] b) {
+        // Implement this method
+        return null;
+    }
+
+    public double[] determinant(double[][][] a) {
+        // Implement this method
+        return null;
+    }
+
+    public double[][] inverse(double[][] a) {
+        // Implement this method
+        return null;
+    }
+
+    public String removeSpecialCharacters(String string) {
+        // Implement this method
+        return null;
+    }
+
+    public boolean isPalindrome(String string) {
+        // Implement this method
+        return true;
+    }
+
+    public int[] generatePrimes(int lower, int upper) {
+        // Implement this method
+        return null;
+    }
+}
+hello world
+fiehfghdu
+
+
