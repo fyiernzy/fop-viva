8e4a260683b7ccfc6053133d3f8fb8c478f8c81c
ndriannazriel04 <22002299@siswa.um.edu.my>
2023-12-12 12:46:56 +0800

    Latest uodates

--- Y2324/Viva2/cout/Solution.java
+++ Y2324/Viva2/cout/Solution.java
@@ -13,14 +13,32 @@ public class Solution {
         return arr;
     }
 
-    public double[][] subtraction(double[][] a, double[][] b) {
-        // Implement this method
-        return null;
+    public static double[][] subtraction(double[][] arr1, double[][] arr2) {
+        double[][] arr = new double[arr1.length][arr1[0].length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[i].length; j++) {
+                arr[i][j] = arr1[i][j] - arr2[i][j];
+            }
+        }
+        return arr;
     }
 
-    public double[][] multiplication(double[][] a, double[][] b) {
-        // Implement this method
-        return null;
+    public static double[][] multiplication(double[][] arr1, double[][] arr2) {
+        int m = arr1.length;
+        int n = arr2.length;
+        int p = arr1.length;
+
+        double[][] arr = new double[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < p; k++) {
+                    arr[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+        return arr;
     }
 
     public double[] determinant(double[][][] a) {
