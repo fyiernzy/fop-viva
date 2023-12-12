a274dbe3fb128e5a96c564d15da537c90d94ddc2
ndriannazriel04 <22002299@siswa.um.edu.my>
2023-12-12 13:04:11 +0800

    LATEST

--- Y2324/Viva2/cout/Solution.java
+++ Y2324/Viva2/cout/Solution.java
@@ -6,7 +6,7 @@ public class Solution {
         double[][] arr = new double[arr1.length][arr1.length];
 
         for (int i = 0; i < arr1.length; i++) {
-            for (int j = 0; j < arr1[i].length; j++) {
+            for (int j = 0; j < arr1[i].length; j++) { 
                 arr[i][j] = arr1[i][j] + arr2[i][j];
             }
         }
